d0c9e080dff9c48b25f16ccec900f6bdc44a30f4
evan70 <ivanpaldan@gmail.com>
2021-01-10 23:40:05 +0100

    initial commit

---
+++ src/components/category-gallery/category-gallery.tsx
@@ -0,0 +1,29 @@
+import { Component, h, State, Host } from '@stencil/core';
+import { Category, getAllCategories } from '../../utils/categoryUtils';
+
+@Component({
+    tag: 'category-gallery',
+    styleUrl: 'category-gallery.scss'
+})
+export class CategoryGallery {
+    @State() categories: Category[] = [];
+
+    async componentWillLoad() {
+        let response = await getAllCategories();
+        this.categories = response.sort((a, b) => a.sortOrder - b.sortOrder);
+    }
+
+    render() {
+        return (
+            <Host class="category-gallery">
+                {this.categories.length
+                    ? <ks-gallery heading="Product Line" class="categories" item-width="350px">
+                          {this.categories.map(x => <ks-card img-src={x.imageUrl} alt={x.name} href={`products/${x.urlSegment}`} class="shadow-none" img-aspect-ratio="2:1" clickable>
+                              <h3 class="mt-auto text-center">{x.name}</h3>
+                          </ks-card>)}
+                      </ks-gallery>
+                    : ''}
+            </Host>
+        );
+    }
+}
---
+++ src/utils/categoryUtils.ts
@@ -0,0 +1,22 @@
+import * as fetch from './fetch';
+import categoryStore from '../store/categoryStore';
+
+export type Category = {
+    id: number,
+    name: string,
+    imageUrl: string,
+    urlSegment: string,
+    description: string,
+    sortOrder: number
+};
+
+export async function getAllCategories(): Promise<Category[]> {
+    let categories = categoryStore.categories;
+
+    if(!categories.length) {
+        categories = await fetch.get<Category[]>('/assets/data/categories.json');
+        categoryStore.categories = categories;
+    }
+
+    return categories;
+}
